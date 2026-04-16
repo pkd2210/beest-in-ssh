19244dc3b4f2baef54b9d948552d6c5f93c27ede
Peleg <45825608+pkd2210@users.noreply.github.com>
2026-04-16 13:10:13 +0300

    Add Shop command

---
+++ commands/shop.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'shop',
+    description: 'List all shop items.',
+    async execute(stream, auth_token, refreshToken) {
+        const response = await fetch('https://beest.hackclub.com/api/shop', {
+                method: 'GET',
+                headers: {
+                    'Cookie' : `auth_token=${auth_token}; refresh_token=${refreshToken}`
+                }
+        })
+        if (response.status === 200) {
+            const data = await response.json();
+            stream.write('\r\nAvailable shop items:\r\n');
+            data.forEach(item => {
+                const description = item.description.replace(/\n/g, ' ');
+                stream.write(`  ${item.name} - ${description} - ${item.priceHours} pipes ${item.stock != null ? `- Stock: ${item.stock}` : ''}\r\n`);
+            });
+        } else {
+            stream.write('\r\nFailed to fetch shop items. Please try again later.\r\n');
+        }
+    }
+}
